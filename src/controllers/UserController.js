5f6afe926dff9fb5cbbac44b18b7305c88f3a9fa
Gabriel Barreto <gabriel.limabarreto@hotmail.com>
2020-05-26 18:08:46 -0300

    adicionando rota post

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -5,5 +5,13 @@ module.exports = {
         const users = await knex('users');
     
         return res.json(users);
+    },
+    async create(req, res) {
+        const { username } = req.body
+        const users = await knex('users').insert({
+            username
+        });
+
+        return res.json(users);
     }
 }
