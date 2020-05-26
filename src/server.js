1d0dc4b1dc5d1382b396fe264fb1a3a6fad3dc3a
Gabriel Barreto <gabriel.limabarreto@hotmail.com>
2020-05-26 17:54:14 -0300

    configurando rotas

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+app.use(require('./routes.js'));
 
 app.listen(3333, () => console.log("Servidor Rodando..."));
