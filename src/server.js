f5a8a3e6abccc4086925ca0e21b61495b3588ed5
Gabriel Barreto <gabriel.limabarreto@hotmail.com>
2020-05-26 19:08:39 -0300

    Tratamento de erro e metodos controller

--- src/server.js
+++ src/server.js
@@ -4,4 +4,9 @@ const app = express();
 app.use(express.json());
 app.use(require('./routes.js'));
 
+// catch all middleware
+app.use((error ,req, res, next) => {
+    res.status(error.status || 500)
+})
+
 app.listen(3333, () => console.log("Servidor Rodando..."));
