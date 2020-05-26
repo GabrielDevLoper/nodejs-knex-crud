12f61853677a3660a79039f6993f7929cd42523d
Gabriel Barreto <gabriel.limabarreto@hotmail.com>
2020-05-26 19:11:48 -0300

    rotas e metodos

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,17 +1,52 @@
 const knex = require('../database');
 
 module.exports = {
-    async index(req, res) {
-        const users = await knex('users');
+    async index(req, res, next) {
+        try {
+            const users = await knex('users');
     
-        return res.json(users);
+            return res.status(200).json(users);
+        } catch (error) {
+            next(error)
+        } 
     },
-    async create(req, res) {
-        const { username } = req.body
-        const users = await knex('users').insert({
-            username
-        });
+    async create(req, res, next) {
+        try {
+            const { username } = req.body
 
-        return res.json(users);
+            const users = await knex('users').insert({
+                username
+            });
+    
+            return res.status(201).json(users);
+        } catch (error) {
+            next(error);
+        }    
+    },
+    async update(req, res, next){
+        try {
+            const { id } = req.params;
+            const { username } = req.body;
+
+            const users = await knex('users').update({
+                username
+            }).where({id});
+
+            return res.json({message: "Alterado com Sucesso!"});
+        } catch (error) {
+            next(error)
+        }
+    },
+
+    async delete(req, res, next) {
+        try {
+            const { id } = req.params;
+            
+            await knex('users').delete().where({ id });
+
+            return res.json({message: "Excluído com Sucesso!"});
+        } catch (error) {
+            next(error);
+        }
     }
 }
--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ app.use(require('./routes.js'));
 // catch all middleware
 app.use((error ,req, res, next) => {
     res.status(error.status || 500)
+    res.json({ error: error.message })
 })
 
 app.listen(3333, () => console.log("Servidor Rodando..."));
