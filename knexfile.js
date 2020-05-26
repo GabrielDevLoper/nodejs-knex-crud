27ac217ddd7001fef79852f4eb9e992b799873eb
Gabriel Barreto <gabriel.limabarreto@hotmail.com>
2020-05-26 16:51:39 -0300

    configurações do knex

---
+++ knexfile.js
@@ -0,0 +1,12 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      database: "node-knex-crud",
+      user: "postgres",
+      password: ""
+    }
+  },
+};
