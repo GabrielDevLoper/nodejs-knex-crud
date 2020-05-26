31abee77498d6a1a3ed44af6933fafa68973ca9d
Gabriel Barreto <gabriel.limabarreto@hotmail.com>
2020-05-26 20:50:21 -0300

    relacionamento da tabela projects com a users

--- src/controllers/ProjectController.js
+++ src/controllers/ProjectController.js
@@ -3,9 +3,19 @@ const knex = require('../database');
 module.exports = {
     async index(req, res, next) {
         try {
-            const projects = await knex('projects');
+            const { user_id } = req.query;
+            const projects =  knex('projects');
+
+            if(user_id) {
+                projects
+                    .where({ user_id })
+                    .join('users', 'users.id', '=', 'projects.user_id')
+                    .select('projects.id', 'users.username', 'projects.title')
+            }
+
+            const data = await projects;
     
-            return res.json(projects);
+            return res.json(data);
         } catch (error) {
             next(error)
         } 
